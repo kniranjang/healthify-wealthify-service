4368ee68a387401183d038672ae16819cc9ba0a6
K Niranjan Ganapathi <kniranjang1995@gmail.com>
2018-09-16 15:08:08 +0530

    Pending changes

--- routes/user_Details.js
+++ routes/user_Details.js
@@ -13,55 +13,67 @@ router
         if (typeof arr !== 'undefined' && arr.length > 0) {
             res.status(httpStatusCodes.OK)
                 .end(JSON.stringify(arr));
-        }
-        res.status(httpStatusCodes.NOT_FOUND)
-            .end("");
+        } else
+            res.status(httpStatusCodes.NOT_FOUND)
+                .end("");
     })
     .get('/:id', function (req, res) {
         var userId = parseInt(req.params.id, 10);
         if (isNaN(userId)) {
             res.status(httpStatusCodes.BAD_REQUEST).end("Id should be an integer");
+        } else {
+            var user = dataClient.getUserById(userId);
+            if (typeof user !== 'undefined')
+                res.status(httpStatusCodes.OK).end(JSON.stringify(user));
+            else
+                res.status(httpStatusCodes.NOT_FOUND).end("");
         }
-        var user = dataClient.getUserById(userId);
-        if (typeof user !== 'undefined')
-            res.status(httpStatusCodes.OK).end(JSON.stringify(user));
-        res.status(httpStatusCodes.NOT_FOUND).end("");
+
     })
     .post('/', function (req, res) {
         var user = req.body;
         if (!validator.IsValidUser(user)[0]) {
             res.status(httpStatusCodes.BAD_REQUEST).end(validator.IsValidUser(user)[1]);
+        } else {
+            user = dataClient.postUser(user);
+            if (typeof user === 'undefined') {
+                res.status(httpStatusCodes.INTERNAL_SERVER_ERROR).end("Failed to post user");
+            } else
+                res.status(httpStatusCodes.OK).end(JSON.stringify(user));
         }
-        user = dataClient.postUser(user);
-        if (typeof user === 'undefined') {
-            res.status(httpStatusCodes.INTERNAL_SERVER_ERROR).end("Failed to post user");
-        }
-        res.status(httpStatusCodes.OK).end(JSON.stringify(user));
+
     })
     .delete('/:id', function (req, res) {
         var userId = parseInt(req.params.id, 10);
         if (isNaN(userId)) {
             res.status(httpStatusCodes.BAD_REQUEST).end("Id should be an integer");
+        } else {
+            var result = dataClient.deleteUser(userId);
+            if (result)
+                res.status(httpStatusCodes.OK).end("Deleted");
+            else
+                res.status(httpStatusCodes.NOT_FOUND).end("Not found");
         }
-        var result = dataClient.deleteUser(userId);
-        if (result)
-            res.status(httpStatusCodes.OK).end("Deleted");
-        res.status(httpStatusCodes.NOT_FOUND).end("Not found");
+
     })
     .put('/:id', function (req, res) {
         var userId = parseInt(req.params.id, 10);
         if (isNaN(userId)) {
             res.status(httpStatusCodes.BAD_REQUEST).end("Id should be an integer");
+        } else {
+            var user = req.body;
+            if (!validator.IsValidUser(user)[0]) {
+                res.status(httpStatusCodes.BAD_REQUEST).end(validator.IsValidUser(user)[1]);
+            } else {
+                user = dataClient.putUser(user, userId);
+                if (typeof user === 'undefined') {
+                    res.status(httpStatusCodes.INTERNAL_SERVER_ERROR).end("Failed to put user.")
+                } else
+                    res.status(httpStatusCodes.OK).end("Updated");
+            }
+
         }
-        var user = req.body;
-        if (!validator.IsValidUser(user)[0]) {
-            res.status(httpStatusCodes.BAD_REQUEST).end(validator.IsValidUser(user)[1]);
-        }
-        user = dataClient.putUser(user, userId);
-        if(typeof user === 'undefined'){
-            res.status(httpStatusCodes.INTERNAL_SERVER_ERROR).end("Failed to put user.")
-        }
-        res.status(httpStatusCodes.OK).end("Updated");
+
     });
 
 module.exports = router;

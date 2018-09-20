664490551bf3264877aaf3e11d5877e9e53261a5
Lipika Mahajan <lmahajan@vistaprint.com>
2018-09-20 16:09:38 +0530

    taken fresh pull from master and added my code over it

---
+++ config.js
@@ -0,0 +1,71 @@
+var config = {}
+
+config.endpoint = "https://healthifywealthify.documents.azure.com:443/";
+config.primaryKey = "GSN9KhqLTcQW9CD5DNqZqLrdyBr7v8wesYzNmLOvKCihAMrbEqp7swNjK3f3KGbltip8Shb6EueS4P1HOMhAeQ==";
+
+config.database = {
+    "id": "HealthyWealthy"
+};
+
+config.collection = {
+    "id": "HealthyWealthyCollection"
+};
+
+config.documents = {
+    "Andersen": {
+        "id": "Anderson.1",
+        "lastName": "Andersen",
+        "parents": [{
+            "firstName": "Thomas"
+        }, {
+                "firstName": "Mary Kay"
+            }],
+        "children": [{
+            "firstName": "Henriette Thaulow",
+            "gender": "female",
+            "grade": 5,
+            "pets": [{
+                "givenName": "Fluffy"
+            }]
+        }],
+        "address": {
+            "state": "WA",
+            "county": "King",
+            "city": "Seattle"
+        }
+    },
+    "Wakefield": {
+        "id": "Wakefield.7",
+        "parents": [{
+            "familyName": "Wakefield",
+            "firstName": "Robin"
+        }, {
+                "familyName": "Miller",
+                "firstName": "Ben"
+            }],
+        "children": [{
+            "familyName": "Merriam",
+            "firstName": "Jesse",
+            "gender": "female",
+            "grade": 8,
+            "pets": [{
+                "givenName": "Goofy"
+            }, {
+                    "givenName": "Shadow"
+                }]
+        }, {
+                "familyName": "Miller",
+                "firstName": "Lisa",
+                "gender": "female",
+                "grade": 1
+            }],
+        "address": {
+            "state": "NY",
+            "county": "Manhattan",
+            "city": "NY"
+        },
+        "isRegistered": false
+    }
+};
+
+module.exports = config;
--- routes/user_Details.js
+++ routes/user_Details.js
@@ -7,98 +7,124 @@ var Validator = require('../lib/validator');
 var validator = new Validator();
 var jwt = require('jsonwebtoken');
 var fs = require('fs');
+var persistence = require('./persistence');
 
 
 /* GET users listing. */
 router
     .get('/', function (req, res) {
-        var arr = dataClient.getAllUsers();
-        if (typeof arr !== 'undefined' && arr.length > 0) {
-            res.status(httpStatusCodes.OK)
-                .end(JSON.stringify(arr));
-        } else
-            res.status(httpStatusCodes.NOT_FOUND)
-                .end("");
-    })
-    .get('/:id', function (req, res) {
-        var token = req.headers.authorization;
-        var secret = process.env.SECRET;
-        try {
-
-
-            if (jwt.verify(token, secret) === req.params.id) {
-                {
-                    var user = dataClient.getUserById(req.params.id);
-                    if (typeof user !== 'undefined')
-                        res.status(httpStatusCodes.OK).end(JSON.stringify(user));
-                    else
-                        res.status(httpStatusCodes.NOT_FOUND).end("");
-                }
-            } else {
-                res.status(httpStatusCodes.UNAUTHORIZED).end("Unauthorized");
-            }
-        }
-        catch (err) {
-            res.status(httpStatusCodes.UNAUTHORIZED).end(err);
-        }
-
+        persistence.queryCollection()
+            .then((arr) => {
+                if (typeof arr !== 'undefined' && arr.length > 0) {
+                    console.log("TEst3");
+                    res.status(httpStatusCodes.OK)
+                        .end(JSON.stringify(arr));
+                } else
+                    res.status(httpStatusCodes.NOT_FOUND)
+                        .end("");
+            });
+        //var arr = dataClient.getAllUsers();
 
     })
+
+     .get('/:id', function (req, res) {
+        console.log("query br id");
+        // var token = req.headers.authorization;
+        // var secret = process.env.SECRET;
+        // try {
+
+
+        //     if (jwt.verify(token, secret) === req.params.id) {
+        //         {
+                    persistence.getDocumentById(req.params.id)
+                        .then((arr) => {
+                            console.log("success");
+                            //arr = JSON.parse(arr[0]);
+                            if (typeof arr !== 'undefined')
+                                res.status(httpStatusCodes.OK).end(JSON.stringify(arr));
+                            else
+                                res.status(httpStatusCodes.NOT_FOUND).end("Not found");
+                        });
+             //   }
+            // } else {
+            //     res.status(httpStatusCodes.UNAUTHORIZED).end("Unauthorized");
+            // }
+        // }
+        // catch (err) {
+        //     res.status(httpStatusCodes.UNAUTHORIZED).end(err);
+        // }
+
+
+    })
+
     .post('/', function (req, res) {
         var user = req.body;
         if (!validator.IsValidUser(user)[0]) {
             res.status(httpStatusCodes.BAD_REQUEST).end(validator.IsValidUser(user)[1]);
         } else {
-            user = dataClient.postUser(user);
-            if (typeof user === 'undefined') {
-                res.status(httpStatusCodes.CONFLICT).end('Email already registered.');
-            } else
-                res.status(httpStatusCodes.OK).end(JSON.stringify(user));
+            //user = dataClient.postUser(user);
+            persistence.createUser(user)
+                .then((arr) => {
+                    if (typeof arr === 'undefined') {
+                        res.status(httpStatusCodes.CONFLICT).end('Email already registered.');
+                    } else {
+                        res.status(httpStatusCodes.OK).end(JSON.stringify(arr));
+                    }
+                });
+
+
         }
 
     })
+
     .delete('/:id', function (req, res) {
         var token = req.headers.authorization;
         var secret = process.env.SECRET;
+        console.log("test1");
         try {
-            if (jwt.verify(token, secret) === req.params.id) {
+            // if (jwt.verify(token, secret) === req.params.id) {
 
-                var result = dataClient.deleteUser(req.params.id);
-                if (result)
-                    res.status(httpStatusCodes.OK).end("Deleted");
-                else
-                    res.status(httpStatusCodes.NOT_FOUND).end("Not found");
+            //var result = dataClient.deleteUser(req.params.id);
 
-            } else {
-                res.status(httpStatusCodes.UNAUTHORIZED).end("Unauthorized");
-            }
+            console.log(req.params.id);
+            persistence.deleteDocument(req.params.id)
+                .then((arr) => {
+                    if (arr)
+                        res.status(httpStatusCodes.OK).end("Deleted");
+                    else
+                        res.status(httpStatusCodes.NOT_FOUND).end("Not found");
+                });
+            // } else {
+            //     res.status(httpStatusCodes.UNAUTHORIZED).end("Unauthorized");
+            // }
         } catch (err) {
-            res.status(httpStatusCodes.UNAUTHORIZED).end(err);
+            res.status(httpStatusCodes.UNAUTHORIZED).end("unauthorized");
         }
-
-
     })
+
+
+
     .put('/:id', function (req, res) {
-        var token = req.headers.authorization;
-        var secret = process.env.SECRET;
-        try {
-            if (jwt.verify(token, secret) === req.params.id) {
-                var user = req.body;
-                if (!validator.IsValidUser(user)[0]) {
-                    res.status(httpStatusCodes.BAD_REQUEST).end(validator.IsValidUser(user)[1]);
-                } else {
-                    user = dataClient.putUser(user, req.params.id);
-                    if (typeof user === 'undefined') {
+        // var token = req.headers.authorization;
+        // var secret = process.env.SECRET;
+        // try {
+        //     if (jwt.verify(token, secret) === req.params.id) {
+
+        var user = req.body;
+        console.log(user);
+        if (!validator.IsValidUser(user)[0]) {
+            res.status(httpStatusCodes.BAD_REQUEST).end(validator.IsValidUser(user)[1]);
+        } else {
+            //user = dataClient.putUser(user, req.params.id);
+            persistence.replaceUser(user, req.params.id)
+                .then((arr) => {
+                    if (typeof arr === 'undefined') {
                         res.status(httpStatusCodes.NOT_FOUND).end("User not found.")
-                    } else
+                    } else {
                         res.status(httpStatusCodes.OK).end("Updated");
-                }
-            } else {
-                res.status(httpStatusCodes.UNAUTHORIZED).end("Unauthorized");
-            }
-        } catch (err) {
-            res.status(httpStatusCodes.UNAUTHORIZED).end(JSON.stringify(err));
+                    }
+                });
+
         }
     });
-
 module.exports = router;
